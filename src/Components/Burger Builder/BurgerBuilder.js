118115af0a5b60497473e15cfa549b4fdf33aa7b
Mohammad Akbar <mohammad.akbar2603@gmail.com>
2021-07-12 16:28:56 +0600

    Add/Remove/Reset items

--- src/Components/Burger Builder/BurgerBuilder.js
+++ src/Components/Burger Builder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Burger from './Burger/Burger';
+import Controls from './Controls/Controls';
 
 export default class BurgerBuilder extends Component {
     state = {
@@ -10,11 +11,48 @@ export default class BurgerBuilder extends Component {
 
         ]
     }
+
+    addIngredientHandle = type => {
+        const ingredients = [...this.state.ingredients];
+        for (let i of ingredients) {
+            if (i.type === type) {
+
+                i.amount++;
+            }
+        }
+        this.setState({
+            ingredients: ingredients
+        })
+    }
+
+    removeIngredientHandle = type => {
+        const ingredients = [...this.state.ingredients];
+        for (let i of ingredients) {
+            if (i.type === type) {
+                if (i.amount <= 0) return;
+                i.amount--;
+            }
+        }
+        this.setState({
+            ingredients: ingredients
+        });
+    }
+
+    resetIngredientHandle = () => {
+        const ingredients = [...this.state.ingredients];
+        for (let i of ingredients) {
+            i.amount = 0;
+        }
+        this.setState({
+            ingredients: ingredients
+        });
+    }
     render() {
         console.log(this.state.ingredients);
         return (
-            <div>
+            <div className="d-flex flex-md-row flex-column">
                 <Burger ingredients={this.state.ingredients} />
+                <Controls ingredientAdded={this.addIngredientHandle} ingredientRemoved={this.removeIngredientHandle} ingredientReset={this.resetIngredientHandle} />
             </div>
         )
     }
---
+++ src/Components/Burger Builder/Controls/Controls.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Card, CardBody, CardHeader, CardFooter } from 'reactstrap';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
+]
+
+const BuildControl = props => {
+    return (
+        <div className='d-flex'>
+            <div className='m-auto ml-5' style={{ fontWeight: 'bold', fontSize: '1.2rem' }}>{props.label}</div>
+            <button onClick={props.removed} className='btn btn-danger btn-sm m-1'>Less</button>
+            <button onClick={props.added} className='btn btn-success btn-sm m-1'>More</button>
+        </div>
+    )
+}
+
+const ResetBuild = props => {
+    return (
+        <div className='d-flex flex-row-reverse my-2'>
+            <button onClick={props.reset} type='button' className='btn btn-light btn-sm mt-2 border border-dark'>Reset</button>
+        </div>
+    )
+}
+
+const Controls = props => {
+    return (
+        <div className='container ml-md-5' style={{ textAlign: 'center' }}>
+            <Card style={{ marginTop: '30px', marginBottom: '30px', textAlign: 'center' }}>
+                <CardHeader style={{ backgroundColor: '#d70f64', color: 'white' }}><h4>Add Ingredients</h4></CardHeader>
+                <CardBody>
+                    {
+                        controls.map(item => {
+                            return <BuildControl
+                                label={item.label}
+                                type={item.type}
+                                key={Math.random()}
+                                added={() => props.ingredientAdded(item.type)}
+                                removed={() => props.ingredientRemoved(item.type)} />
+                        })
+                    }
+                    <ResetBuild reset={() => props.ingredientReset()} />
+
+                </CardBody>
+                <CardFooter><h5>Price: BDT</h5></CardFooter>
+            </Card>
+        </div>
+    )
+}
+
+export default Controls;
