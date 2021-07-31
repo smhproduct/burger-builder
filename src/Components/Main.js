35d2f6be4ee2ef54982d45d9d176e4e69849eccc
Mohammad Akbar <mohammad.akbar2603@gmail.com>
2021-07-31 13:28:28 +0600

    Added localStorage for storing token,so that refreshing the site doesnt cause to logout automatically

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,6 +6,8 @@ import Checkout from './Orders/Checkout/Checkout';
 import Auth from './Auth/Auth';
 import { Redirect, Route, Switch } from 'react-router-dom';//IMPORTANT:: auto import e react-router theke import hoi, but react-router-dom is recommended, so manually korte hobe eta
 import { connect } from 'react-redux';
+import { authCheck } from '../redux/authActionCreators';
+import { Component } from 'react';
 
 const mapStateToProps = state => {
     return {
@@ -13,34 +15,54 @@ const mapStateToProps = state => {
     }
 }
 
-const Main = props => {
-    let routes = null;
-    if (props.token === null) {
-        routes = (
-            <Switch>
-                <Route path="/login" component={Auth} />
-                <Redirect to='/login' />
-            </Switch>
-        )
-    } else {
-
-        routes = (<Switch>
-            <Route path='/orders' component={Orders} />
-            <Route path='/checkout' component={Checkout} />
-            <Route path='/' exact component={BurgerBuilder} />
-            <Redirect to='/' />
-        </Switch>)
+const mapDispatchToProps = dispatch => {
+    return {
+        authCheck: () => dispatch(authCheck()),
     }
-    return (
-        <div>
-            <Header />
-            <div className='container'>
-                {routes}
 
-            </div>
-
-        </div>
-    )
 }
 
-export default connect(mapStateToProps)(Main);
+class Main extends Component {
+    componentDidMount() {
+        this.props.authCheck();
+    }
+    render() {
+        let routes = null;
+        if (this.props.token === null) {
+            routes = (
+                <Switch>
+                    <Route path="/login" component={Auth} />
+                    <Redirect to='/login' />
+                </Switch>
+            )
+        } else {
+
+            routes = (<Switch>
+                <Route path='/orders' component={Orders} />
+                <Route path='/checkout' component={Checkout} />
+                <Route path='/' exact component={BurgerBuilder} />
+                <Redirect to='/' />
+            </Switch>)
+        }
+        return (
+            <div>
+                <Header />
+                <div className='container'>
+                    {routes}
+
+                </div>
+
+            </div>
+        )
+    }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
+
+//MUST READ
+//authCheck ta ekhan theke dispatched hobe, but dispatch howar specific kono conditional rendering nai Main() functional comp er bhitor
+//Tai amra ei compo ta mount er shathe shahe call korte chai authCheck ke
+//But eta functional component e kora possible na
+//So amra full compo ta ke class compo te convert korsi to use compoDidMount
+//N.B: func compo te reducer theke asha state ke props.blahblah likhtam, but class compo kore felse dekhe this.props.blahblah likhte hobe
--- src/redux/authActionCreators.js
+++ src/redux/authActionCreators.js
@@ -24,9 +24,35 @@ export const auth = (email, password, mode) => dispatch => {
     const API_KEY = "AIzaSyBIo11_zZyQCBLn4ibjf834vSQBKrBN3hc";
     axios.post(authUrl + API_KEY, authData)
         .then(response => {
+            localStorage.setItem('token', response.data.idToken);
+            localStorage.setItem('userId', response.data.localId);
+            const expirationTime = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+            localStorage.setItem('expirationTime', expirationTime);
             dispatch(authSuccess(response.data.idToken, response.data.localId))
         })
 }
+
+export const authCheck = () => dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        //Logout
+    }
+    else {
+        const expirationTime = new Date(localStorage.getItem('expirationTime'));
+        const currentTime = new Date();
+        if (expirationTime <= currentTime) {
+            //Logout
+        }
+        else {
+            const userId = localStorage.getItem('userId');
+            dispatch(authSuccess(token, userId))
+        }
+    }
+}
+//MUST READ:
+//Ei je authCheck() function ta banaisi eta kintu website load er shomoi joto early/root e possible okhane set korte hobe
+//main root hocche App.js; but okhane ami just Main compo ke call korsi, linear dekhe Main.js keo ami root compo consider korte parbo
+//ei function taile Main.js e call hobe
 
 //TEST ACCOUNT(Signed up)
 //m@g.co   
